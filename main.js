c053232b09f601582815a394e03cf43b7c5e93d5
adamjc <adamjc86@gmail.com>
2019-02-08 22:06:22 +0000

    Refactor + new colours + optimised algorithm
    - We now only change direction if the current direction is much worse than the next best direction
    - Changed to 80s vibes colours
    - Cleaned up how neighbours are found

--- main.js
+++ main.js
@@ -1,25 +1,14 @@
-function removeNode (nodeIndex, set) {
-  return set.slice(0, nodeIndex).concat(set.slice(nodeIndex + 1, set.length))
-}
+const removeNode = (nodeIndex, set) =>set.slice(0, nodeIndex).concat(set.slice(nodeIndex + 1, set.length))
 
-// Manhattan distance
 const manhattan = (a, b) => Math.abs(a.x - b.x) + Math.abs(a.y - b.y)
-
-// Euclidean distance
 const euclidean = (a, b) => Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2))
 
-function findCost (a, b, heuristic = 'euclidean') {
-  const heuristics = {
-    manhattan,
-    euclidean
-  }
-
-  return heuristics[heuristic](a, b)
-}
+const findCost = (a, b, heuristic = euclidean) => heuristic(a, b)
 
 function addNeighbour (node, set) {
   for (let i = 0; i < set.length; i += 1) {
-    if (node.f <= set[i].f) {
+    // The +1 ensures that the best node is *much* better than our current direction
+    if (node.f <= (set[i].f + 1)) {
       return set.slice(0, i).concat(node).concat(set.slice(i, set.length))
     }
   }
@@ -28,68 +17,54 @@ function addNeighbour (node, set) {
 }
 
 function findNeighbours (node, set) {
-  // we are in a maze, you can only move in one of four directions, so your neighbours are above, below, left and right.
-  let neighbours = []
+  const x = set[node.x]
+  const leftX = set[node.x - 1]
+  const rightX = set[node.x + 1]
 
-  // left
-  if (set[node.x - 1] && set[node.x - 1][node.y] && !set[node.x - 1][node.y].isWall) {
-    neighbours.push(set[node.x - 1][node.y])
-  }
+  const leftUp = leftX ? leftX[node.y - 1] : null
+  const up = x[node.y - 1] ? x[node.y - 1] : null
+  const rightUp = rightX ? rightX[node.y - 1] : null
+  const left = leftX ? leftX[node.y] : null
+  const right = rightX ? rightX[node.y] : null
+  const leftDown = leftX ? leftX[node.y + 1] : null
+  const down = x[node.y + 1] ? x[node.y + 1] : null
+  const rightDown = rightX ? rightX[node.y + 1] : null
 
-  // left up
-  if (set[node.x - 1] && set[node.x - 1][node.y - 1] && !set[node.x - 1][node.y - 1].isWall) {
-    neighbours.push(set[node.x - 1][node.y - 1])
-  }
+  const neighbours = [
+    leftUp, up, rightUp, left, right, leftDown, down, rightDown
+  ]
 
-  // left down
-  if (set[node.x - 1] && set[node.x - 1][node.y + 1] && !set[node.x - 1][node.y + 1].isWall) {
-    neighbours.push(set[node.x - 1][node.y + 1])
-  }
-
-  // right
-  if (set[node.x + 1] && set[node.x + 1][node.y] && !set[node.x + 1][node.y].isWall) {
-    neighbours.push(set[node.x + 1][node.y])
-  }
-
-  // right up
-  if (set[node.x + 1] && set[node.x + 1][node.y - 1] && !set[node.x + 1][node.y - 1].isWall) {
-    neighbours.push(set[node.x + 1][node.y - 1])
-  }
-
-  // right down
-  if (set[node.x + 1] && set[node.x + 1][node.y + 1] && !set[node.x + 1][node.y + 1].isWall) {
-    neighbours.push(set[node.x + 1][node.y + 1])
-  }
-
-  // up
-  if (set[node.x][node.y - 1] && !set[node.x][node.y - 1].isWall) {
-    neighbours.push(set[node.x][node.y - 1])
-  }
-
-  // down
-  if (set[node.x][node.y + 1] && !set[node.x][node.y + 1].isWall) {
-    neighbours.push(set[node.x][node.y + 1])
-  }
-
-  return neighbours
+  return neighbours.map(neighbour => {
+    if (neighbour && !neighbour.isWall) return neighbour
+  }).filter(el => el)
 }
 
 function drawPath (node) {
   const nodeSize = (width / nodes.length) - 1
   
-  fill(0, 0, 255)
-  square(node.x * nodeSize, node.y * nodeSize, nodeSize)
-  
+  stroke('#fff')
+  strokeWeight(4)
+
   if (node.bestNode) {
+    const startX = (node.x * nodeSize) + (nodeSize / 2)
+    const startY = (node.y * nodeSize) + (nodeSize / 2)
+    const endX = (node.bestNode.x * nodeSize) + (nodeSize / 2)
+    const endY = (node.bestNode.y * nodeSize) + (nodeSize / 2)
+    line(startX, startY, endX, endY)
     drawPath(node.bestNode)
   }
 }
 
-const size = 25
+function drawNode (x, y, width, radius = 0) {
+  square(x, y, width, radius, radius, radius, radius)
+}
+
+const size = 100
 const nodes = new Array(size).fill()
                                .map((_, x) => {
-                                 return new Array(size).fill().map((_, y) => ({ x, y, isWall: Math.random(1) < 0.3 }))
+                                 return new Array(size).fill().map((_, y) => ({ x, y, isWall: Math.random(1) < 0.2}))
                                })
+
 const start = nodes[0][0]
 const end = nodes[size - 1][size - 1]
 
@@ -102,43 +77,47 @@ if (end.isWall) {
 
 let openSet = [start]
 let closedSet = []
+let counter = 0
 
 function setup () {
-  createCanvas(500, 500)
+  createCanvas(windowHeight, windowHeight)
 }
 
 function draw () {
+  noStroke()
+  counter += 1
   const nodeSize = (width / nodes.length) - 1
   // draw all nodes
   for (let x = 0; x < nodes.length; x += 1) {
     for (let y = 0; y < nodes[x].length; y += 1) {
-      if (nodes[x][y].isWall) fill (0, 0, 0)
+      if (nodes[x][y].isWall) fill('#444')
       else fill(255, 255, 255)
 
-      square(x * nodeSize, y * nodeSize, nodeSize)
+      drawNode(x * nodeSize, y * nodeSize, nodeSize)
     }
   }
 
   // draw all nodes in the open set
   openSet.forEach(node => {
-    fill(0, 255, 0)
-    square(node.x * nodeSize, node.y * nodeSize, nodeSize)
+    fill('#2bd1fc')
+    drawNode(node.x * nodeSize, node.y * nodeSize, nodeSize)
   })
 
   // draw all nodes in the closed set  
   closedSet.forEach(node => {
-    fill(255, 0, 0)
-    square(node.x * nodeSize, node.y * nodeSize, nodeSize)
+    fill('#ff48c4')
+    drawNode(node.x * nodeSize, node.y * nodeSize, nodeSize)
   })
 
   if (openSet.length) {
     const currentNode = openSet.shift()
-  
+    drawPath(currentNode)
     if (currentNode === end) {
       // found the end node, make the path
-      console.log('Found end node')
+      console.log(`Found end node in: ${counter}`)
+
       console.log(currentNode)
-      drawPath(currentNode)
+      // drawPath(currentNode)
       noLoop()
     }
     
@@ -148,30 +127,27 @@ function draw () {
     const neighbours = findNeighbours(currentNode, nodes)
     
     for (let i = 0; i < neighbours.length; i++) {
-      // if neighbour is in closed set, ignore it
       const neighbour = neighbours[i]
 
       if (closedSet.includes(neighbour)) continue
-            
+
       const newG = currentNode.g + findCost(currentNode, neighbour)
-      
+
       if (!openSet.includes(neighbour)) {
         neighbour.g = newG
-        neighbour.f = newG +  findCost(neighbour, end)
+        neighbour.f = newG + findCost(neighbour, end)
         neighbour.bestNode = currentNode
         openSet = addNeighbour(neighbour, openSet)
-        // openSet.push(neighbour)
-        // sort openSet...
-      } else if (newG > neighbour.g) {
+      } else if (newG >= neighbour.g) {
         continue
       }
 
       neighbour.g = newG
-      neighbour.f = newG +  findCost(neighbour, end)
+      neighbour.f = newG + findCost(neighbour, end)
       neighbour.bestNode = currentNode
     }
   } else {
-    console.log('No solution found')
+    console.log(`No solution found in: ${counter}`) 
     noLoop()
   }
 }
